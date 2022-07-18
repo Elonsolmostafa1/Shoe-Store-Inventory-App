55327e08790a49e7afa268fa079aff8df8db638d
Elonsolmostafa1 <mostafaahmed21121997@gmail.com>
2022-07-18 16:50:03 +0200

    Login fragment UI

--- Shoe Store/starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ Shoe Store/starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,21 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+      //  val navController = this.findNavController(R.id.myNavHostFragment)
+        //NavigationUI.setupActionBarWithNavController(this, navController)
         Timber.plant(Timber.DebugTree())
     }
+
+
 }
